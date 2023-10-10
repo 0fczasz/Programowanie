604b5e4de2da744fdc0b46477e74b91f67da29e0
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-10-10 16:19:10 +0200

    przdwdzaslp

--- Programowanie/InstrucionIfConsoleApplication/InstrucionIfConsoleApplication.cpp
+++ Programowanie/InstrucionIfConsoleApplication/InstrucionIfConsoleApplication.cpp
@@ -12,8 +12,7 @@ Operatory porównania:
 
 */
 
-/*
-*/
+
 
 
 //Napisz funkcję, która podzieli dwie liczby.
@@ -109,22 +108,59 @@ void task5() {
         cout << "Nie poprawne hasło";
     }
 }
-//6. Program sprawdzający czy podana data jest poprawna (np. sprawdzając, czy dzień jest z zakresu od 1 do 31, miesiąc od 1 do 12 itd.)
-void task6(){
-    int day, month;
+//6. Program sprawdzający czy podana data jest poprawna(np.sprawdzając, czy dzień jest z zakresu od 1 do 31, miesiąc od 1 do 12 itd.)
+void task6()
+{
+	int day;
+	int month;
+	int year;
+	cout << "podaj dzień \n" << endl;
+	cin >> day;
+	cout << "Podaj miesiąc \n" << endl;
+	cin >> month;
+	cout << "Podaj rok \n" << endl;
+	cin >> year;
+    if (month >= 1 && month <= 12)
+    {
+        if (month == 2)
+        {
+            if (year % 4 == 0 && year % 100 > 0 || year % 400 == 0)
+            {
+                if (day < 30)
+                {
+                    cout << "data jest poprawna";
+                }
+                else
+                {
+                    cout << "data jest nie poprawna";
+                }
 
-    cout << "Podaj dzień \n";
-    cin >> day;
-    cout << "Podaj miesiąc \n";
-    cin >> month;
-    if (day > 0 && day < 32 && month > 0 && month < 13) {
-        cout << "Poprawna data";
+            }
+            else if (year % 4 != 0 && year % 100 > 1 || year % 400 != 0)
+            {
+                if (day < 29) {
+                    cout << "data jest poprawna";
+                }
+                else
+                {
+                    cout << "data jest nie poprwana";
+                }
+            }
+
+        }
+        else if ((month == 4 || month == 6 || month == 9 || month == 11) && day >= 1 && day <= 30)
+        {
+            cout << "data jest poprawna" << endl;
+        }
+        else if (day >= 1 && day <= 31)
+        {
+            cout << "data jest nie poprawna" << endl;
+        }
+        else
+        {
+            cout << "data jest nie poprawna" << endl;
+        }
     }
-    else {
-
-        cout << "Nie poprawna data";
-    }
-
 }
 //7. Program wyświetlający odpowiedni komunikat w zależności od podanej temperatury (np. "ciepło" dla temperatury powyżej 20 stopni Celsjusza, "chłodno" dla temperatury poniżej 10 stopni Celsjusza itd.)
 void task7()
@@ -140,6 +176,66 @@ void task7()
         cout << "Chłodno";
     }
 }
+
+// 8. Program sprawdzjacy czy podana liczba jest z przediału < 1; 15)
+void task8()
+{
+    double Number;
+
+    cout << "Podaj liczbe \n";
+    cin >> Number;
+    if (Number => 1 && Number < 15)
+    {
+        cout << "Podana liczba jest z przedziału <1, 15)";
+    }
+    else
+    {
+        cout << "Podana liczba nie jest z tego przedziału";
+    }
+
+}
+
+//9. Program sprawdzający czy osoba jest pełnoletnia.
+void task9()
+{
+    double years;
+    
+    cout << "Podaj wiek osoby \n";
+    cin >> years;
+    if (years > 18)
+    {
+        cout << "Osoba nie jest pełnoletnia";
+    }
+    else
+    {
+        cout << "Osoba jest pełnoletnia";
+    }
+}   
+/*
+10. Program, który sprawdzi czy z podanych długości boków można zrobić trójkąt.
+*/
+void task10()
+{
+    double triangleSide1;
+    double triangleSide2;
+    double triangleSide3;
+    
+    cout << "Podaj pierwszy bok trójkąta \n";
+    cin >> triangleSide1;
+    cout << "Podaj pierwszy bok trójkąta \n";
+    cin >> triangleSide2;
+    cout << "Podaj pierwszy bok trójkąta \n";
+    cin >> triangleSide3;
+
+    if ((triangleSide1 + triangleSide2) > triangleSide3 && (triangleSide1 + triangleSide3) > triangleSide2 && (triangleSide2 + triangleSide3) > triangleSide1)
+    {
+        cout << "Z podanych długości boków można zrobić trójkąt";
+    }
+    else
+    {
+        cout << "Z podanych długości boków nie można zrobić trójkąt";
+    }
+}
 int main()
 {
     setlocale(LC_CTYPE, "polish");
@@ -151,4 +247,7 @@ int main()
     //task5();
     //task6();
     //task7();
+    //task8();
+    //task9();
+    //task10();
 }
