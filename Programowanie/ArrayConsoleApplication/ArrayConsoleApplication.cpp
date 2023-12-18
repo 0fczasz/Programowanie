68d2831daa3546b61944349e1e01da80c6620260
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-12-18 11:26:05 +0100

    xcv

--- Programowanie/ArrayConsoleApplication/ArrayConsoleApplication.cpp
+++ Programowanie/ArrayConsoleApplication/ArrayConsoleApplication.cpp
@@ -15,10 +15,45 @@ void task1()
     {
         cout << "Podana liczbaa" << numbers[i] << "\n";
     }
+ 
+}
+void task2()
+{
+    const int UPPER_RANGE = 100;
+    const int LOWER_RANGE = 5;
+
+    const int SIZE_OF_ARRAY = 5;
+    int numbers[SIZE_OF_ARRAY];
+
+    srand(time(NULL));
+
+    for (int i = 0; i < SIZE_OF_ARRAY; i++)
+    {
+        numbers[i] = rand() % (UPPER_RANGE - LOWER_RANGE + 1) + LOWER_RANGE;
+    }
+
+    for (int i = 0; i < SIZE_OF_ARRAY; i++)
+        std::cout << numbers[i] << ", ";
+    std::cout << "\n";
+    
+    //szukanie maksimum
+    int max = numbers[0];
+    for (int i = 1; i < SIZE_OF_ARRAY; i++)
+        if (numbers[i] > max)
+            max = numbers[i];
+    //szukanie min
+    int min = numbers[0];
+    for (int i = 1; i < SIZE_OF_ARRAY; i++)
+        if (numbers[i] < min)
+            min = numbers[i];
+    cout << "najwieksza to " << max << "\n";
+    cout << "najmniejsza to " << min << "\n";
 }
 int main()
 {
-    task1();
+    //task1();
+    task2();
+
 }
 
 
