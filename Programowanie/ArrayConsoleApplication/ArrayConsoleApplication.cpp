42458ff5f00c76b9b8d776b5ef1bf16d0a9d8a78
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-01-05 13:07:40 +0100

    jhk

--- Programowanie/ArrayConsoleApplication/ArrayConsoleApplication.cpp
+++ Programowanie/ArrayConsoleApplication/ArrayConsoleApplication.cpp
@@ -49,10 +49,72 @@ void task2()
     cout << "najwieksza to " << max << "\n";
     cout << "najmniejsza to " << min << "\n";
 }
+void task3()
+{
+    const int UPPER_RANGE = 100;
+    const int LOWER_RANGE = 5;
+
+    const int SIZE_OF_ARRAY = 5;
+    int numbers[SIZE_OF_ARRAY];
+
+    srand(time(NULL));
+
+    for (int i = 0; i < SIZE_OF_ARRAY; i++)
+    {
+        numbers[i] = rand() % (UPPER_RANGE - LOWER_RANGE + 1) + LOWER_RANGE;
+    }
+
+    for (int i = 0; i < SIZE_OF_ARRAY; i++)
+        std::cout << numbers[i] << ", ";
+    std::cout << "\n";
+
+    int sum = 0;
+
+    for (int i = 0; i < SIZE_OF_ARRAY; i++)
+    {
+        sum = sum + numbers[i];
+    }
+    double average = sum * 1.0 / SIZE_OF_ARRAY;
+    cout << "Œrednia liczb to " << average << "\n";
+}
+
+void task4()
+{
+    const int UPPER_RANGE = 100;
+    const int LOWER_RANGE = 5;
+
+    const int SIZE_OF_ARRAY = 5;
+    int numbers[SIZE_OF_ARRAY];
+
+    srand(time(NULL));
+
+    for (int i = 0; i < SIZE_OF_ARRAY; i++)
+    {
+        numbers[i] = rand() % (UPPER_RANGE - LOWER_RANGE + 1) + LOWER_RANGE;
+    }
+
+    for (int i = 0; i < SIZE_OF_ARRAY; i++)
+        std::cout << numbers[i] << ", ";
+    std::cout << "\n";
+    
+    //wersja 1
+    for (int numberFromRange = LOWER_RANGE; numberFromRange < UPPER_RANGE; numberFromRange++)
+    {
+        int numberOfOccurences = 0;
+        for (int j = 0; j < SIZE_OF_ARRAY; j++)
+        {
+            if (numbers[j] == numberFromRange)
+                numberOfOccurences++;
+        }
+        cout << "liczba " << numberFromRange << "wystapila " << numberOfOccurences << "razy\n";
+    }
+}
 int main()
 {
     //task1();
-    task2();
+    //task2();
+    //task3();
+    task4();
 
 }
 
