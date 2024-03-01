3fc77d3e8cd5d578ae957b170d3921a9f2db7dbb
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-03-01 13:00:02 +0100

    d

--- Programowanie/FirstObjectConsoleApplication/FirstObjectConsoleApplication.cpp
+++ Programowanie/FirstObjectConsoleApplication/FirstObjectConsoleApplication.cpp
@@ -1,34 +1,31 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
 class Point
 {
 private:
-	int x;
-	int y;
-	int z;
+    int x;
+    int y;
+    int z;
+
 public:
-	//void SetX
 };
+
 double distanceFromCenter(Point p)
 {
-	double distance = sqrt(p.x * p.x + p.y * p.y + p.z * p.z);
-	return distance;
-
+    double distance;
+    distance = sqrt(p.x * p.x + p.y * p.y + p.z * p.z);
+    return distance;
 }
 
 int main()
 {
-	setlocale(LC_CTYPE, "polish");
-	
+    setlocale(LC_CTYPE, "polish");
 
-	Point firstPoint;
-	firstPoint.x = 3;
-	firstPoint.y = 5;
-	firstPoint.z = 8;
+    Point firstPoint;
+    firstPoint.x = 3;
+    firstPoint.y = 5;
+    firstPoint.z = 8;
 
-	cout << "Odleglosc " << distanceFromCenter(firstPoint) << "\n";
-
-
+    cout << "Odleg³oœæ " << distanceFromCenter(firstPoint) << "\n";
 }
-
