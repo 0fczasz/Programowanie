90b35bb61bbed3207550629dd418821ed4ad1c34
q <q>
2024-09-27 11:38:47 +0200

    efg

---
+++ Programowanie/structConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿Point firstPoint = new Point();
+firstPoint.x = 5;
+firstPoint.y = 10;
+Point secondPoint = new Point();
+secondPoint = firstPoint;
+secondPoint.x = 6;
+secondPoint.y = 99;
+Console.WriteLine(firstPoint.x);
+Console.WriteLine(secondPoint.x);
