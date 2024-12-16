e47e032691fd2742bf08f4d341030ab4b625b7ec
Uczeń <Ucze@DESKTOP-RGP55N4>
2024-12-16 11:40:15 +0100

    32

---
+++ Programowanie/Egzamin1ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿  int IleKostki;
+    do
+    {
+        Console.WriteLine("Ile kostek chcesz rzucić? (3 - 10)");
+        IleKostki = int.Parse(Console.ReadLine());
+    } while (IleKostki < 3 || IleKostki > 10);
+
+
+
+void OczkaKostki()
+{
+
+    Random random = new Random();
+    for(int i = 1; i <= IleKostki; i++)
+    {
+        Console.WriteLine($"Kostka {i}: " );
+    }
+}
+
+OczkaKostki();
