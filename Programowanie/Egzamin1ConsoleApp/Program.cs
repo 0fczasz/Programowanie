8e2b2ed38745e423993732320f8077d3c3567bb3
Uczeń <Ucze@DESKTOP-RGP55N4>
2024-12-17 15:59:49 +0100

    dsa

--- Programowanie/Egzamin1ConsoleApp/Program.cs
+++ Programowanie/Egzamin1ConsoleApp/Program.cs
@@ -1,20 +1,31 @@
 ﻿  int IleKostki;
+    void IleRzucic()
+{
     do
     {
         Console.WriteLine("Ile kostek chcesz rzucić? (3 - 10)");
         IleKostki = int.Parse(Console.ReadLine());
     } while (IleKostki < 3 || IleKostki > 10);
-
-
+}
 
 void OczkaKostki()
 {
-
+    IleRzucic();
+    int sumaRandom = 0;
     Random random = new Random();
     for(int i = 1; i <= IleKostki; i++)
     {
-        Console.WriteLine($"Kostka {i}: " );
+        Console.WriteLine($"Kostka {i}: {random.Next(1,7)}" );
+        sumaRandom += random.Next(1,7);
     }
+    Console.WriteLine(sumaRandom);
 }
 
-OczkaKostki();
+string JeszczeRaz;
+
+do
+{
+    Console.WriteLine("Jeszcze raz? (t/n)");
+    JeszczeRaz = Console.ReadLine();
+    OczkaKostki();
+} while (JeszczeRaz == "t");
