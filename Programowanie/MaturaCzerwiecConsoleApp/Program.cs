35bdc5871f4b04afe50a2adc41f8650e8efd3ffe
q <q>
2025-02-10 11:36:11 +0100

    d

--- Programowanie/MaturaCzerwiecConsoleApp/Program.cs
+++ Programowanie/MaturaCzerwiecConsoleApp/Program.cs
@@ -18,3 +18,14 @@ var reverseDovidedBy17 = strNumbers
 Console.WriteLine("Zdanie 4.1:");
 foreach (var strNumber in reverseDovidedBy17)
     Console.WriteLine(strNumber);
+
+
+Console.WriteLine("zadanie 4.2");
+
+var strNumberWithMaxDiffrence = strNumbers.OrderBy(s => Math.Abs(int.Parse(s) - int.Parse(string.Join("", s.Reverse())))).LastOrDefault();
+
+Console.Write(strNumberWithMaxDiffrence);
+Console.Write(" ");
+Console.WriteLine(Math.Abs(int.Parse(strNumberWithMaxDiffrence) - int.Parse(string.Join("", strNumberWithMaxDiffrence.Reverse()))));
+
+Console.WriteLine("zadanie 4.3");
