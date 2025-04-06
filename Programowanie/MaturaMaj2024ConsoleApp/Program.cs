737796c9241b4bb8d8a233afef64e295cfe45e7e
0fczasz <eyve00@gmail.com>
2025-04-06 19:48:13 +0200

    zadania 3.2, 3.3

--- Programowanie/MaturaMaj2024ConsoleApp/Program.cs
+++ Programowanie/MaturaMaj2024ConsoleApp/Program.cs
@@ -1,41 +1,98 @@
 ﻿using static System.Math;
 StreamReader streamReader = new StreamReader("skrot_przyklad.txt");
-string num;
-streamReader.ReadLine();
 List<string> numbers = new List<string>();
-while ((num = streamReader.ReadLine()) != null)
+string? linia = streamReader.ReadLine();
+while (linia != null)
 {
-    numbers.Add(num);
+    numbers.Add(linia);
+    linia = streamReader.ReadLine();
 }
-streamReader.Close();
 
+Console.WriteLine("Zadanie 3.2");
 
-//3.2
-int total = 0;
-int max = 0;
-int ma2x = 0;
+int? nieparzystySkrot(string number)
+{
+    string skrot = "";
+    foreach (char num in number)
+    {
+        if (num % 2 == 1)
+        {
+            skrot = skrot + num;
+        }
+        
+    }
+    if (skrot == "")
+        return null;
+    else
+        return int.Parse(skrot);
+}
 
+int najwiekszaLiczba = 0;
+int ileLiczb = 0;
 foreach (string number in numbers)
 {
-    bool isGood = true;
-    int numberInt = int.Parse(number);
-    for (int i = 1; i <= number.Length; i++)
+
+    if (nieparzystySkrot(number) == null)
     {
-        int n = numberInt % (int)Pow(10, i);
-        numberInt -= n;
-        n = n / (int)Pow(10, i - 1);
-        if (n == 1 || n == 3 || n == 5 || n == 7 || n == 9)
-        {
-            isGood = false;
-            break;
-        }
-    }
-    if (isGood)
-    {
-        total++;
-        if (int.Parse(number) > max)
-            max = int.Parse(number);
+        int dobraLiczba = int.Parse(number);
+        ileLiczb++;
+        if (dobraLiczba > najwiekszaLiczba)
+            najwiekszaLiczba = dobraLiczba;
     }
 }
-Console.WriteLine(total);
-Console.WriteLine(max);
+
+Console.WriteLine(ileLiczb);
+Console.WriteLine(najwiekszaLiczba);
+
+
+// 3.3
+Console.WriteLine("Zadanie 3.3");
+
+    StreamReader streamReader2 = new StreamReader("skrot2_przyklad.txt");
+List<string> numbers2 = new List<string>();
+string? linia2 = streamReader2.ReadLine();
+while (linia2 != null)
+{
+    numbers2.Add(linia2);
+    linia2 = streamReader2.ReadLine();
+}
+
+
+foreach (string number2 in numbers2){
+    int? skrot = nieparzystySkrot(number2);
+    if (skrot != null)
+    {
+        int wynikNWD = NWD(int.Parse(number2), (int) skrot);
+        if (wynikNWD == 7)
+        {
+            Console.WriteLine(number2);
+        }
+    }
+}
+
+
+int NWD(int a, int b)
+{
+  
+    while (a % b != 0)
+    {
+        int mod = a % b;
+        a = b;
+        b = mod;
+    }
+    return b;
+}
+
+
+
+
+
+
+//List<int> results = File.ReadAllLines("skrot_przyklad.txt")
+//    .Where(line => line.All(character => character % 2 == 0))
+//    .Select(line => int.Parse(line))
+//    .ToList();
+
+
+//Console.WriteLine(results.Count);
+//Console.WriteLine(results.Max());
