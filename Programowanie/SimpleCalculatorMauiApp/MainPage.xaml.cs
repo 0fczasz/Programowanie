1b0f8b17816579ec8a0fe02a8cfea97b3574d978
Sala213 <Sala213@ezse.pl>
2025-09-10 11:35:00 +0200

    d

---
+++ Programowanie/SimpleCalculatorMauiApp/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleCalculatorMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
