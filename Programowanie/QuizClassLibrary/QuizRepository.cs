c87c1a35f0e047f255e8315c5d6680fd23b97adb
Sala213 <Sala213@ezse.pl>
2026-03-18 13:02:22 +0100

    NO

---
+++ Programowanie/QuizClassLibrary/QuizRepository.cs
@@ -0,0 +1,24 @@
+﻿using QuizClassLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuizReposytoryClassLibrary.DTO;
+
+namespace QuizClassLibrary
+{
+    public class QuizReposytory
+    {
+        private QuizDBContext context;
+        public QuizReposytory()
+        {
+            context = new QuizDBContext();
+        }
+
+        public QuestionDTO GetCurrentQuestion(int currentQuestionId)
+        {
+            return context.Questions.Where(p => p.Id == currentQuestionId).Select(q => new QuestionDTO() { Id = q.Id, QuestionText = q.QuestionText }).First();
+        }
+    }
+}
