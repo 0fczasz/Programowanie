ab8f4057ad926478a3a406ddd3e88786ddb83a1f
Sala213 <Sala213@ezse.pl>
2026-03-18 13:07:50 +0100

    dwa

--- Programowanie/QuizMauiApp/MainNewModel.cs
+++ Programowanie/QuizMauiApp/MainNewModel.cs
@@ -7,6 +7,8 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QuizClassLibrary.DTO;
+using QuizReposytoryClassLibrary.DTO;
 
 namespace QuizMauiApp
 {
